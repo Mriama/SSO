868be684bfe793642e73bd3be1d95fc62d37e4bb
WW930\a810342 <mariama.2.diallo@atos.net>
2022-02-28 21:14:11 +0000

    fixed bug

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -138,4 +138,124 @@ class Utilisateur
     {
         return $this->idUtilisateur;
     }
+
+    public function getCtln(): ?string
+    {
+        return $this->ctln;
+    }
+
+    public function setCtln(string $ctln): self
+    {
+        $this->ctln = $ctln;
+
+        return $this;
+    }
+
+    public function getCtfn(): ?string
+    {
+        return $this->ctfn;
+    }
+
+    public function setCtfn(string $ctfn): self
+    {
+        $this->ctfn = $ctfn;
+
+        return $this;
+    }
+
+    public function getFredurneresp(): ?string
+    {
+        return $this->fredurneresp;
+    }
+
+    public function setFredurneresp(?string $fredurneresp): self
+    {
+        $this->fredurneresp = $fredurneresp;
+
+        return $this;
+    }
+
+    public function getFreduresdel(): ?string
+    {
+        return $this->freduresdel;
+    }
+
+    public function setFreduresdel(?string $freduresdel): self
+    {
+        $this->freduresdel = $freduresdel;
+
+        return $this;
+    }
+
+    public function getFredufonctadm(): ?string
+    {
+        return $this->fredufonctadm;
+    }
+
+    public function setFredufonctadm(?string $fredufonctadm): self
+    {
+        $this->fredufonctadm = $fredufonctadm;
+
+        return $this;
+    }
+
+    public function getFredugestresp(): ?string
+    {
+        return $this->fredugestresp;
+    }
+
+    public function setFredugestresp(?string $fredugestresp): self
+    {
+        $this->fredugestresp = $fredugestresp;
+
+        return $this;
+    }
+
+    public function getCtemail(): ?string
+    {
+        return $this->ctemail;
+    }
+
+    public function setCtemail(?string $ctemail): self
+    {
+        $this->ctemail = $ctemail;
+
+        return $this;
+    }
+
+    public function getCodaca(): ?string
+    {
+        return $this->codaca;
+    }
+
+    public function setCodaca(?string $codaca): self
+    {
+        $this->codaca = $codaca;
+
+        return $this;
+    }
+
+    public function getFreduurlretour(): ?string
+    {
+        return $this->freduurlretour;
+    }
+
+    public function setFreduurlretour(?string $freduurlretour): self
+    {
+        $this->freduurlretour = $freduurlretour;
+
+        return $this;
+    }
+
+    public function getCtgrps(): ?string
+    {
+        return $this->ctgrps;
+    }
+
+    public function setCtgrps(?string $ctgrps): self
+    {
+        $this->ctgrps = $ctgrps;
+
+        return $this;
+    }
 }
